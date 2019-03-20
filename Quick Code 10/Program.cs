36bbc39c3f1450c49b55f0e4ff950085e88427d4
Connor cheung <calebchang991@outlook.com>
2019-03-20 13:56:44 -0400

    Initial Push

---
+++ Quick Code 10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Code_10
+{
+    class Program
+    {
+        public enum Fruit
+        {
+            Rasberry,
+            Strawberry,
+            Blueberry,
+            Blackberry,
+            Apple,
+            Orange,
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            //
+            // Variables
+            //
+            string userResponse;
+            //
+            // Declare Arrays
+            //
+
+            Fruit[] favoriteFruit = new Fruit[3];
+
+
+                Console.WriteLine("What are you three favorite kinds of fruits?");
+
+            for (int index = 0; index < 3; index++)
+            {
+                Console.WriteLine($"Fruit {index +1}: ");
+                userResponse = Console.ReadLine();
+                Enum.TryParse(userResponse, out favoriteFruit[index]);
+
+            }
+            Console.WriteLine();
+
+            foreach (Fruit fruit in favoriteFruit)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+        }
+    }
+}
